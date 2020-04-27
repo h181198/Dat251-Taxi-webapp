b4d2a5c66b2387314ff9bf502b71f5a1564aacda
bendikjohansen <bendikj@gmail.com>
2020-04-27 20:41:41 +0200

    asdkjfldasføasdfø

--- cypress/integration/examples/render_spec.js
+++ cypress/integration/examples/render_spec.js
@@ -5,9 +5,23 @@ describe('Can access parts of website', () => {
       cy.contains("Taxideling")
     })
 
-    it('Can access homepage through /home and contians taxideling', () => {
+    it('Can access homepage through /home and contains taxideling', () => {
+        cy.visit("http://localhost:3000/home")
+        cy.wait(300)
+        cy.contains("Taxideling")
+    })
+
+    it('Can access homepage through /home and contains taxideling', () => {
         cy.visit("http://localhost:3000")
         cy.wait(300)
         cy.contains("Taxideling")
+    })
+
+    it('Gets data from server', () => {
+        cy.visit("http://localhost:3000")
+        cy.wait(300)
+        cy.get('input[list="fromList"]').type('Bryggen');
+        cy.get('#fromList option').should('have.length', 3).
+          first().should('have.value', 'Bryggen');
     })
   })
