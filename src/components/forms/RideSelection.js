87835da84bfbef7bae5127d786ca77595ee91efe
Andreas Evjenth <anstroen@gmail.com>
2020-04-28 08:01:06 +0200

    return if undefined

--- src/components/forms/RideSelection.js
+++ src/components/forms/RideSelection.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const RideSelection = () => {
-    const rides = useSelector(state => state.ride.available);
-    const stops = useSelector(state => state.stop.list);
+  const rides = useSelector((state) => state.ride.available);
+  const stops = useSelector((state) => state.stop.list);
 
-    const formatFromTo = ({startId, stopId}) => {
-        const fromName = stops.find(stop => stop.id === startId);
-        const toName = stops.find(stop => stop.id === stopId);
-
-        return `${fromName.name} → ${toName.name}`;
+  const formatFromTo = ({ startId, stopId }) => {
+    const fromName = stops.find((stop) => stop.id === startId);
+    const toName = stops.find((stop) => stop.id === stopId);
+    if (!fromName || !toName) {
+      return "";
     }
 
-    return (
-        <ul>
-            {rides.map(ride => (
-                <li key={ride.id}>
-                    <p>{formatFromTo(ride)}</p>
-                    <p>{new Date(ride.timeOfDeparture).toUTCString()}</p>
-                </li>
-            ))}
-        </ul>
-    );
+    return `${fromName.name} → ${toName.name}`;
+  };
+
+  return (
+    <ul>
+      {rides.map((ride) => (
+        <li key={ride.id}>
+          <p>{formatFromTo(ride)}</p>
+          <p>{new Date(ride.timeOfDeparture).toUTCString()}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default RideSelection;
